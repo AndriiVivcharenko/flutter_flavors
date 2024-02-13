7eacdedb4c1b0bb91dc5ec6715b07e8da0de1ab1
AndriiVivcharenko <andrii.v@radency.com>
2024-02-13 16:19:36 +0200

    firebase_options_<flavor> files have been removed, as this project will be used only as an example.

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_remote_config/firebase_remote_config.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_flavors/app.dart';
-import 'package:flutter_flavors/database/app_database.dart';
-import 'package:flutter_flavors/database/repositories/events_with_label_repository.dart';
-import 'package:flutter_flavors/repositories/remote_config_repository.dart';
-import 'package:flutter_flavors/utils/white_label_utils.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_flavors/main_common.dart';
 import 'package:flutter_flavors/firebase_options_factory.dart';
 
 void main() async {
-  final WidgetsBinding widgetsBinding =
-      WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-
-  await Firebase.initializeApp(
-    options: FirebaseOptionsFactory().options,
-  );
-
-  final remoteConfigsRepository =
-      RemoteConfigRepository(remoteConfig: FirebaseRemoteConfig.instance);
-  await remoteConfigsRepository.configure();
-
-  FlutterNativeSplash.remove();
-
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        RepositoryProvider<RemoteConfigRepository>(
-          create: (context) => remoteConfigsRepository,
-        ),
-        RepositoryProvider<AppDatabase>(
-          create: (context) => AppDatabase(),
-          lazy: false,
-        ),
-        RepositoryProvider<EventsWithLabelRepository>(
-          create: (context) => EventsWithLabelRepository(
-            database: context.read<AppDatabase>(),
-          ),
-        )
-      ],
-      child: const MyApp(),
-    ),
-  );
+  mainCommon(() async {
+    await Firebase.initializeApp(
+      options: FirebaseOptionsFactory().options,
+    );
+  });
 }
--- lib/main_common.dart
+++ lib/main_common.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_flavors/app.dart';
 import 'package:flutter_flavors/database/app_database.dart';
+import 'package:flutter_flavors/database/repositories/events_with_label_repository.dart';
 import 'package:flutter_flavors/repositories/remote_config_repository.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
@@ -26,6 +27,12 @@ void mainCommon(Future Function() init) async {
         ),
         RepositoryProvider<AppDatabase>(
           create: (context) => AppDatabase(),
+          lazy: false,
+        ),
+        RepositoryProvider<EventsWithLabelRepository>(
+          create: (context) => EventsWithLabelRepository(
+            database: context.read<AppDatabase>(),
+          ),
         ),
       ],
       child: const MyApp(),
